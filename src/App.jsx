a3ffcfef6f5b22c15932f0343264cfa57034525b
Jiantao <144024751+jiantaoshen@users.noreply.github.com>
2026-04-22 16:31:26 +0200

    Update FetchApi URL for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Tracking your packages</h1>
-        <FetchApi UrlAPI="/Project_Package_Tracking/testdata.json" />
+        <FetchApi UrlAPI="/testdata.json" />
+        {/*<FetchApi UrlAPI="/Project_Package_Tracking/testdata.json" /> */}
         {/* <FetchApi UrlAPI="https://my.api.mockaroo.com/orders.json?key=e49e6840" /> */}
       </header>
     </div>
